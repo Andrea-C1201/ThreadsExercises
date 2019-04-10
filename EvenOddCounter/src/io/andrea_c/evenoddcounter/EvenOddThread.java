69cc11eb29ed243786fb3ba27cc129a64947b75b
Andrea-C1201 <giandreas@outlook.it>
2019-04-10 12:38:13 +0200

    Added inline javadocs

--- EvenOddCounter/src/io/andrea_c/evenoddcounter/EvenOddThread.java
+++ EvenOddCounter/src/io/andrea_c/evenoddcounter/EvenOddThread.java
@@ -1,34 +1,94 @@
+/*******************************************************************************
+ * Copyright (C) 2019 Andrea-C1201
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package io.andrea_c.evenoddcounter;
 
 /**
+ * This is the {@link Thread} that will produce incrementally-generated numbers
+ * from 0 to a specific value and says if the generated value is even or odd.
  * 
+ * @see Thread
  * 
  * @author Andrea-C1201 <giandreas@outlook.it>
  */
 public class EvenOddThread extends Thread {
 
+	/**
+	 * This enum is used by {@link EvenOddThread} class. It says to the thread if it
+	 * has to check even numbers or odd one.
+	 * 
+	 * @author Andrea-C1201
+	 */
 	public enum ValueCalculationType {
 		EVEN, ODD;
 	}
 
+	/**
+	 * The delay time, used in the {@link EvenOddThread#run() run} method, by
+	 * {@link Thread#sleep(long) sleep}.
+	 */
 	private final int DELAY = 500;
+
+	/**
+	 * The type of this thread: even or odd.
+	 */
 	private ValueCalculationType numberType;
+
+	/**
+	 * The last number of the sequence.
+	 */
 	private final int finalNumber;
 
+	/**
+	 * Constructor.
+	 * 
+	 * @param number the target number of the counter
+	 * @param type   of the thread: {@link ValueCalculationType#EVEN even} or
+	 *               {@link ValueCalculationType#ODD odd}.
+	 */
 	public EvenOddThread(int number, ValueCalculationType type) {
-		this.finalNumber = number + 1;
+		this.finalNumber = number;
 		this.numberType = type;
 	}
 
+	/**
+	 * <p>
+	 * This is the run method invoked on the {@linkplain Thread#start() thread
+	 * execution}.
+	 * </p>
+	 * 
+	 * <p>
+	 * It count from 0 to {@linkplain EvenOddThread#finalNumber finalNumber} and
+	 * print the process name and if the number is even or odd. Then put this thread
+	 * to the {@link Thread#sleep(long) sleep} status.
+	 * </p>
+	 * 
+	 * @see Runnable
+	 */
 	@Override
 	public void run() {
-		for (int i = 0; i < finalNumber; i++) {
+		// ME!ME!ME! japanese song by Daoko & TeddyLoid
+		String meMeMe = Thread.currentThread().getName();
+		for (int i = 1; i < finalNumber + 1; i++) {
 			if (numberType.equals(ValueCalculationType.EVEN)) {
 				if (i % 2 == 0)
-					System.out.println("Even " + i);
+					System.out.println(meMeMe + " - Even " + i);
 			} else {
 				if (i % 2 != 0)
-					System.out.println("Odd " + i);
+					System.out.println(meMeMe + " - Odd " + i);
 			}
 			try {
 				sleep(DELAY);
--- EvenOddCounter/src/io/andrea_c/evenoddcounter/Main.java
+++ EvenOddCounter/src/io/andrea_c/evenoddcounter/Main.java
@@ -1,14 +1,56 @@
+/*******************************************************************************
+ * Copyright (C) 2019 Andrea-C1201
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
 package io.andrea_c.evenoddcounter;
 
+/**
+ * This is the main class for this exercise (ex.11 pg.129)
+ * 
+ * @author Andrea-C1201 <giandreas@outlook.it>
+ */
 public class Main {
 
+	/**
+	 * <p>
+	 * This is the main of the program.
+	 * </p>
+	 * <p>
+	 * It gets the number from the command line arguments array and tries to convert
+	 * from a string into an integer value.
+	 * </p>
+	 * <p>
+	 * If it succeed, it starts 2 threads, one count the even numbers and the other
+	 * the odd one, from 1 to the inserted number. Then it waits for the threads to
+	 * end. ({@link Thread#join()})
+	 * </p>
+	 * <p>
+	 * If it fail, it enter in the catch block, usually for an
+	 * {@link NumberFormatException} and simple exit the program.
+	 * </p>
+	 * 
+	 * @param args in this case, an integer number
+	 */
 	public static void main(String[] args) {
 		if (args.length != 1) {
 			System.err.println("Usage: java -jar [filename.jar] [NUMBER]");
 			System.err.println("Or add a value to the Debug/Run configuration in your IDE.");
 			return;
 		}
-		
+
 		try {
 			int number = Integer.parseInt(args[0]);
 			Thread even = new EvenOddThread(number, EvenOddThread.ValueCalculationType.EVEN);
